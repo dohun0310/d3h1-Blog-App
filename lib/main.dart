d75e0e5bce307f28762c8516d8ecbadd35dcdac8
dohun0310 <dohun0310@naver.com>
2023-08-09 18:40:14 +0900

    add Tablet layout

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,95 @@ void main() {
   runApp(const MyApp());
 }
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: const Text('d3h1 Blog')),
+//         body: FutureBuilder<List<ArticleList>>(
+//           future: getArticleData(),
+//           
+//             else {
+//                return ListView.builder(
+//                 itemCount: snapshot.data!.length + 1,
+//                 itemBuilder: (context, index) {
+//                   if (index == 0) {
+//                     return const Padding(
+//                       padding: EdgeInsets.all(8.0),
+//                       child: Align(
+//                         alignment: Alignment.centerLeft,
+//                         child: Text(
+//                           "홈",
+//                           style: TextStyle(
+//                             fontSize: 24,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ),
+//                     );
+//                   }
+//                   var article = snapshot.data![index - 1];
+//                   return InkWell(
+//                     onTap: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                           builder: (context) => ArticlePage(link: article.link),
+//                         ),
+//                       );
+//                     },
+//                     child: Container(
+//                       padding: const EdgeInsets.all(8.0),
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: <Widget>[
+//                           Image.network(article.img),
+//                           const SizedBox(height: 8),
+//                           Text(
+//                             article.category,
+//                             style: const TextStyle(
+//                               fontSize: 12,
+//                             ),
+//                           ),
+//                           Text(
+//                             article.title,
+//                             style: const TextStyle(
+//                               fontSize: 18,
+//                               fontWeight: FontWeight.bold,
+//                             ),
+//                           ),
+//                           Text(
+//                             article.description,
+//                             style: const TextStyle(
+//                               fontSize: 14,
+//                               color: Colors.grey,
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   );
+//                 },
+//               );
+//             }
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final isTablet = MediaQuery.of(context).size.shortestSide >= 600;
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('d3h1 Blog')),
@@ -31,11 +115,10 @@ class MyApp extends StatelessWidget {
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(child: CircularProgressIndicator());
-            }
-            else if (snapshot.hasError) {
+            } else if (snapshot.hasError) {
               return Center(
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
                   child: const Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -64,73 +147,148 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               );
-            }
-            else {
-               return ListView.builder(
-                itemCount: snapshot.data!.length + 1,
-                itemBuilder: (context, index) {
-                  if (index == 0) {
-                    return const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "홈",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    );
-                  }
-                  var article = snapshot.data![index - 1];
-                  return InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ArticlePage(link: article.link),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Image.network(article.img),
-                          const SizedBox(height: 8),
-                          Text(
-                            article.category,
-                            style: const TextStyle(
-                              fontSize: 12,
-                            ),
-                          ),
-                          Text(
-                            article.title,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            article.description,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              );
+            } else {
+              if (isTablet || isLandscape) {
+                return _tabletLayout(snapshot.data);
+              } else {
+                return _mobileLayout(snapshot.data);
+              }
             }
           },
         ),
       ),
+    );
+  }
+
+  Widget _mobileLayout(List<ArticleList>? data) {
+      return ListView.builder(
+        itemCount: data!.length + 1,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "홈",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+          }
+          var article = data[index - 1];
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ArticlePage(link: article.link),
+                ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Image.network(article.img),
+                  const SizedBox(height: 8),
+                  Text(
+                    article.category,
+                    style: const TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    article.title,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    article.description,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+  }
+
+
+  Widget _tabletLayout(List<ArticleList>? data) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: data!.length + 1,
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "홈",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          );
+        }
+        var article = data[index - 1];
+        return InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ArticlePage(link: article.link),
+              ),
+            );
+          },
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.network(article.img),
+                const SizedBox(height: 8),
+                Text(
+                  article.category,
+                  style: const TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  article.title,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  article.description,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
@@ -149,11 +307,41 @@ class ArticlePage extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return const Center(
-              child: Text("Error loading article."),
+          }
+          else if (snapshot.hasError) {
+            return Center(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.error),
+                    SizedBox(height: 8),
+                    Text(
+                      "블로그를 불러오는데 오류가 발생했어요.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "인터넷에 연결이 되어있지 않거나\n블로그 자체에 문제가 생긴 것일 수 있어요.\n",
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "인터넷 연결에 문제가 없다면 개발자에게 문의해주세요.",
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
             );
-          } else {
+          }
+          else {
             return SingleChildScrollView(
               padding: const EdgeInsets.all(8.0),
               child: Html(data: snapshot.data ?? "")
@@ -164,7 +352,6 @@ class ArticlePage extends StatelessWidget {
     );
   }
 }
-
 
 Future<List<ArticleList>> getArticleData() async {
   final response = await http.get(Uri.parse('https://blog.d3h1.com'));
@@ -205,9 +392,11 @@ Future<String> getArticleContent(String link) async {
   var document = parser.parse(response.body);
   dom.Element? contentElement = document.querySelector('main article');
 
-  if (contentElement != null) {
-    return contentElement.innerHtml;
-  } else {
-    throw Exception("Failed to parse the article content");
-  }
+  contentElement?.querySelectorAll('img').forEach((imgElement) {
+    String? src = imgElement.attributes['src'];
+    if (src != null && !src.startsWith('http')) {
+      imgElement.attributes['src'] = 'https://blog.d3h1.com$src';
+    }
+  });
+  return contentElement?.outerHtml ?? '';
 }
